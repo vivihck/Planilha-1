86da1ac4720d1e76118b8407ca18431027aff436
vivihck <07victoriadossantos@gmail.com>
2024-07-30 15:49:01 -0300

    Add files via upload

---
+++ cod2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 2;
+        int[] v = new int[7];
+
+        while (a < 6)
+        {
+            v[a] = 10 * a;
+            a = a + 1;
+        }
+
+        for (int cont = 0; cont < 7; cont++)
+        {
+            System.Console.WriteLine($"vetor[{cont}] = {v[cont]}");
+        }
+    }
+}
---
+++ codi1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int c = (a + b) / 2;
+        c = c - 40;
+        int[] v = new int[7];
+
+        v[3] = a + b + c;
+
+        for (int cont = 0; cont < 7; cont++)
+           
+        {
+           
+            System.Console.WriteLine($"vetor[{cont}] = {v[cont]}");
+        }
+    }
+}
